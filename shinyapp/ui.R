70e755a44d5b4efd5bdbcafa762abdd3f16f9e3f
kartoffelsalat <silberzwiebel@users.noreply.github.com>
2018-06-02 14:49:49 +0200

    use full width of well-panel to center button

--- shinyapp/ui.R
+++ shinyapp/ui.R
@@ -47,7 +47,12 @@ shinyUI(
 								max = 24,
 								value = c(0, 24)
 							),
-							fluidRow(column(6, align="center", offset = 3, actionButton("QueryBtn", "Aktualisieren", icon = icon("refresh"))))
+							fluidRow(
+								column(12, 
+								       align="center", 
+								       actionButton("QueryBtn", "Aktualisieren", icon = icon("refresh"))
+								      )
+							)
 						),  # end wellPanel vehicle / location choice
 						tabsetPanel(
 							id = "tabs_time",
